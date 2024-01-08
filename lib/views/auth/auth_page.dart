7329790e6bad8a7f38767b0e9e10505fd46032ae
Nallu-Swami <aringopalan13489@gmail.com>
2024-01-09 03:09:13 +0530

    GET and POST Request work

--- lib/views/auth/auth_page.dart
+++ lib/views/auth/auth_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
 class UserAuthPage extends StatefulWidget {
   const UserAuthPage({super.key});
 
@@ -14,11 +15,25 @@ class _UserAuthPageState extends State<UserAuthPage> {
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
   }
+
+  void postRequest() async {
+    var url = Uri.parse('http://localhost:3000/postsomething');
+    var data = {
+      "Username": "ThulluNalluNonSwami",
+      "Weapon": "DualSwords",
+      "Nature": "Poision",
+      "Personality": "DarkGreen",
+    };
+    var response = await http.post(url, body: data);
+    print(response.body.toString());
+  }
+
   @override
   void initState() {
     super.initState();
-    getRequest();
+    postRequest();
   }
+
   Widget build(BuildContext context) {
     return const Placeholder();
   }
