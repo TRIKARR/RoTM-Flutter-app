5b7489dcf6898eaf43d1642090de30f05c086510
Nallu-Swami <aringopalan13489@gmail.com>
2024-01-09 14:34:32 +0530

    Completed Login and SignUP section UI

--- lib/views/auth/signup_page.dart
+++ lib/views/auth/signup_page.dart
@@ -1,10 +1,11 @@
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rotm/assets/constants.dart';
-import 'package:rotm/views/auth/signup_page.dart';
+import 'package:rotm/views/auth/auth_page.dart';
 
-class UserAuthPage extends StatelessWidget {
-  const UserAuthPage({super.key});
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -27,9 +28,7 @@ class UserAuthPage extends StatelessWidget {
                   child:
                       Image(image: AssetImage("lib/assets/images/login.png")),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
+
                 // Text("Welcome Back BRoTM",
                 //     textAlign: TextAlign.center,
                 //     style: GoogleFonts.getFont(
@@ -41,7 +40,7 @@ class UserAuthPage extends StatelessWidget {
                 // const SizedBox(
                 //   height: 10,
                 // ),
-                Text("BRoTM Login",
+                Text("BRoTM SignUp",
                     textAlign: TextAlign.center,
                     style: GoogleFonts.getFont(
                       'Orbitron',
@@ -49,6 +48,32 @@ class UserAuthPage extends StatelessWidget {
                       color: const Color.fromARGB(255, 51, 1, 117),
                       fontWeight: FontWeight.bold,
                     )),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: TextField(
+                    style: GoogleFonts.getFont(
+                      'Orbitron',
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    decoration: const InputDecoration(
+                      fillColor: Color.fromARGB(255, 157, 240, 230),
+                      filled: true,
+                      hintText: "Email Address",
+                      prefixIcon: Icon(Icons.mail),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
                 const SizedBox(
                   height: 20,
                 ),
@@ -91,7 +116,7 @@ class UserAuthPage extends StatelessWidget {
                     decoration: const InputDecoration(
                       fillColor: Color.fromARGB(255, 157, 240, 230),
                       filled: true,
-                      hintText: "BroTM-Secret",
+                      hintText: "BroTM-Password",
                       prefixIcon: Icon(Icons.key),
                       border: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.black),
@@ -102,20 +127,35 @@ class UserAuthPage extends StatelessWidget {
                     ),
                   ),
                 ),
+                const SizedBox(
+                  height: 20,
+                ),
                 Padding(
-                  padding:
-                      const EdgeInsets.only(right: 35.0, top: 10, bottom: 10),
-                  child: Container(
-                    alignment: Alignment.topRight,
-                    child: Text(
-                      "Forgot Password?",
-                      style: GoogleFonts.orbitron(
-                        fontWeight: FontWeight.bold, // Make the font bold
-                        color: const Color.fromARGB(
-                            255, 51, 1, 117), // Set the font color
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: TextField(
+                    obscureText: true,
+                    style: GoogleFonts.getFont(
+                      'Orbitron',
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    decoration: const InputDecoration(
+                      fillColor: Color.fromARGB(255, 157, 240, 230),
+                      filled: true,
+                      hintText: "Confirm BroTM-Password",
+                      prefixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30),
+                        ),
                       ),
                     ),
                   ),
+                ),
+                const SizedBox(
+                  height: 20,
                 ),
                 Center(
                   child: TextButton(
@@ -139,96 +179,7 @@ class UserAuthPage extends StatelessWidget {
                         ),
                       )),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    children: [
-                      const Expanded(
-                          child: Divider(
-                        color: Colors.black,
-                      )),
-                      Text("Continue with",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.getFont(
-                            'Orbitron',
-                            fontSize: 15,
-                            color: const Color.fromARGB(255, 51, 1, 117),
-                            fontWeight: FontWeight.bold,
-                          )),
-                      const Expanded(
-                          child: Divider(
-                        color: Colors.black,
-                      )),
-                    ],
-                  ),
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 80,
-                      ),
-                      child: Container(
-                        height: 70,
-                        width: 70,
-                        padding: const EdgeInsets.all(
-                            8.0), // Add padding to prevent the image from touching the borders
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(
-                              255, 18, 31, 29), // Set the background color
-                          border: Border.all(
-                            color: const Color.fromARGB(255, 255, 255, 255),
-                          ),
-                          borderRadius: BorderRadius.circular(20),
-                          image: const DecorationImage(
-                            image: AssetImage("lib/assets/images/logo.png"),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20, right: 20),
-                      child: Container(
-                        height: 70,
-                        width: 70,
-                        padding: const EdgeInsets.all(
-                            8.0), // Add padding to prevent the image from touching the borders
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(
-                              255, 18, 31, 29), // Set the background color
-                          border: Border.all(
-                            color: const Color.fromARGB(255, 255, 255, 255),
-                          ),
-                          borderRadius: BorderRadius.circular(20),
-                          image: const DecorationImage(
-                            image: AssetImage("lib/assets/images/google.png"),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        right: 10,
-                      ),
-                      child: Container(
-                        height: 70,
-                        width:
-                            70, // Add padding to prevent the image from touching the borders
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(
-                              255, 18, 31, 29), // Set the background color
-                          border: Border.all(
-                            color: const Color.fromARGB(255, 255, 255, 255),
-                          ),
-                          borderRadius: BorderRadius.circular(34),
-                          image: const DecorationImage(
-                            image: AssetImage("lib/assets/images/p.jpg"),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+
                 const SizedBox(
                   height: 20,
                 ),
@@ -238,7 +189,7 @@ class UserAuthPage extends StatelessWidget {
                         child: Divider(
                       color: Colors.black,
                     )),
-                    Text("Not a BRoTM yet ?",
+                    Text("Alrady a BRoTM ?",
                         textAlign: TextAlign.center,
                         style: GoogleFonts.getFont(
                           'Orbitron',
@@ -270,12 +221,11 @@ class UserAuthPage extends StatelessWidget {
                       onPressed: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(
-                              builder: (context) => const SignUpPage()),
+                          MaterialPageRoute(builder: (context) => const UserAuthPage()),
                         );
                       },
                       child: const Text(
-                        "REGISTER",
+                        "SIGN-IN",
                         style: TextStyle(
                           fontFamily: 'Orbitron',
                           fontSize: 20,
