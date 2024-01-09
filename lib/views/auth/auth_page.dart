acdb45e86bf258f1e0c22fb2465da1a0551e8025
Nallu-Swami <aringopalan13489@gmail.com>
2024-01-09 12:46:30 +0530

    Login finished , SignUp started

--- lib/views/auth/auth_page.dart
+++ lib/views/auth/auth_page.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rotm/assets/constants.dart';
@@ -16,7 +14,7 @@ class UserAuthPage extends StatelessWidget {
           gradient: LinearGradient(
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
-              colors: [g1, g2, g3, g4]),
+              colors: [g1, g2, g3, g1]),
         ),
         child: SingleChildScrollView(
           child: Padding(
@@ -31,18 +29,18 @@ class UserAuthPage extends StatelessWidget {
                 const SizedBox(
                   height: 10,
                 ),
-                Text("Welcome Back BRoTM",
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.getFont(
-                      'Orbitron',
-                      fontSize: 25,
-                      color: const Color.fromARGB(255, 51, 1, 117),
-                      fontWeight: FontWeight.bold,
-                    )),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text("Login",
+                // Text("Welcome Back BRoTM",
+                //     textAlign: TextAlign.center,
+                //     style: GoogleFonts.getFont(
+                //       'Orbitron',
+                //       fontSize: 25,
+                //       color: const Color.fromARGB(255, 51, 1, 117),
+                //       fontWeight: FontWeight.bold,
+                //     )),
+                // const SizedBox(
+                //   height: 10,
+                // ),
+                Text("BRoTM Login",
                     textAlign: TextAlign.center,
                     style: GoogleFonts.getFont(
                       'Orbitron',
@@ -51,7 +49,7 @@ class UserAuthPage extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     )),
                 const SizedBox(
-                  height: 30,
+                  height: 20,
                 ),
                 Padding(
                   padding: const EdgeInsets.only(left: 10, right: 10),
@@ -63,12 +61,12 @@ class UserAuthPage extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                     decoration: const InputDecoration(
-                      fillColor: Colors.white,
+                      fillColor: Color.fromARGB(255, 157, 240, 230),
                       filled: true,
-                      hintText: "Enter your BroTM ID",
+                      hintText: "BroTM-ID",
                       prefixIcon: Icon(Icons.person),
                       border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
+                        borderSide: BorderSide(color: Colors.black),
                         borderRadius: BorderRadius.all(
                           Radius.circular(30),
                         ),
@@ -77,11 +75,12 @@ class UserAuthPage extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(
-                  height: 30,
+                  height: 20,
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  padding: const EdgeInsets.only(left: 10, right: 10),
                   child: TextField(
+                    obscureText: true,
                     style: GoogleFonts.getFont(
                       'Orbitron',
                       fontSize: 18,
@@ -89,18 +88,168 @@ class UserAuthPage extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                     decoration: const InputDecoration(
-                      fillColor: Colors.white,
+                      fillColor: Color.fromARGB(255, 157, 240, 230),
                       filled: true,
-                      hintText: "Enter your BroTM Secret",
+                      hintText: "BroTM-Secret",
                       prefixIcon: Icon(Icons.key),
                       border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
+                        borderSide: BorderSide(color: Colors.black),
                         borderRadius: BorderRadius.all(
                           Radius.circular(30),
                         ),
                       ),
                     ),
                   ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(right: 35.0, top: 10, bottom: 10),
+                  child: Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                      "Forgot Password?",
+                      style: GoogleFonts.orbitron(
+                        fontWeight: FontWeight.bold, // Make the font bold
+                        color: const Color.fromARGB(
+                            255, 51, 1, 117), // Set the font color
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: TextButton(
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor:
+                            const Color.fromARGB(255, 28, 215, 194),
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                            side: BorderSide(color: Colors.black)),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "PROCEED",
+                        style: TextStyle(
+                          fontFamily: 'Orbitron',
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      const Expanded(
+                          child: Divider(
+                        color: Colors.black,
+                      )),
+                      Text("Continue with",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.getFont(
+                            'Orbitron',
+                            fontSize: 15,
+                            color: const Color.fromARGB(255, 51, 1, 117),
+                            fontWeight: FontWeight.bold,
+                          )),
+                      const Expanded(
+                          child: Divider(
+                        color: Colors.black,
+                      )),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 80,
+                      ),
+                      child: Container(
+                        height: 70,
+                        width: 70,
+                        padding: const EdgeInsets.all(
+                            8.0), // Add padding to prevent the image from touching the borders
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(
+                              255, 18, 31, 29), // Set the background color
+                          border: Border.all(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                          image: const DecorationImage(
+                            image: AssetImage("lib/assets/images/logo.png"),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: Container(
+                        height: 70,
+                        width: 70,
+                        padding: const EdgeInsets.all(
+                            8.0), // Add padding to prevent the image from touching the borders
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(
+                              255, 18, 31, 29), // Set the background color
+                          border: Border.all(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                          image: const DecorationImage(
+                            image: AssetImage("lib/assets/images/logo.png"),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        right: 10,
+                      ),
+                      child: Container(
+                        height: 70,
+                        width:
+                            70, // Add padding to prevent the image from touching the borders
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(
+                              255, 18, 31, 29), // Set the background color
+                          border: Border.all(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                          image: const DecorationImage(
+                            image: AssetImage("lib/assets/images/logo.png"),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    const Expanded(
+                        child: Divider(
+                      color: Colors.black,
+                    )),
+                    Text("Not a BRoTM yet ?",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.getFont(
+                          'Orbitron',
+                          fontSize: 15,
+                          color: const Color.fromARGB(255, 51, 1, 117),
+                          fontWeight: FontWeight.bold,
+                        )),
+                    const Expanded(
+                        child: Divider(
+                      color: Colors.black,
+                    )),
+                  ],
                 ),
               ],
             ),
