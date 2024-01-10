1c25c71d08e1f1eff6e051a5cac84bf00b8d0473
Nallu-Swami <aringopalan13489@gmail.com>
2024-01-10 19:56:27 +0530

    Speed change NavBAr

--- lib/views/user/bottom_nav_bar.dart
+++ lib/views/user/bottom_nav_bar.dart
@@ -16,6 +16,7 @@ class _BottomNavComponentState extends State<BottomNavComponent> {
       bottomNavigationBar: CurvedNavigationBar(
         backgroundColor: Colors.deepPurple.shade900,
         color: Colors.cyan.shade300,
+        animationDuration: const Duration(milliseconds: 400),
         items: const [
           Icon(Icons.home),
           Icon(Icons.home),
