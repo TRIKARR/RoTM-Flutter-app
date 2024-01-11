2ff12605b1aeb5ee5b31078276a7e2673afcec87
Nallu-Swami <aringopalan13489@gmail.com>
2024-01-11 18:07:10 +0530

    Testing random ui 2

--- lib/models/week_data.dart
+++ lib/models/week_data.dart
@@ -11,6 +11,10 @@ class _WeekDataSectionState extends State<WeekDataSection> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(0),
+        color: const Color.fromARGB(255, 97, 77, 187),
+      ),
       child: Text("Week data section"),
     );
   }
