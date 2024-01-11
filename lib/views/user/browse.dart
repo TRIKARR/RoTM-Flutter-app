247fd645f1373a7cfef3d3fb0b1babed54663ac6
Nallu-Swami <aringopalan13489@gmail.com>
2024-01-11 21:23:55 +0530

    Report section started

--- lib/views/user/browse.dart
+++ lib/views/user/browse.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class UserBrowseMenu extends StatefulWidget {
   const UserBrowseMenu({super.key});
@@ -12,6 +13,32 @@ class _UserBrowseMenuState extends State<UserBrowseMenu> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple.shade900,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            leading: IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                "lib/assets/animated/magazine.gif",
+              ),
+            ),
+            title: Text("REPORT",
+                style: GoogleFonts.getFont(
+                  'Orbitron',
+                  fontSize: 30,
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontWeight: FontWeight.bold,
+                )),
+            automaticallyImplyLeading: false,
+            expandedHeight: 350,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                color: const Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
