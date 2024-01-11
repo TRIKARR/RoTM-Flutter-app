5c1825a9a5424c68fd00d00945dc39d6b76e4351
Nallu-Swami <aringopalan13489@gmail.com>
2024-01-11 15:01:28 +0530

    Changing routes

--- lib/views/user/bottom_nav_bar.dart
+++ lib/views/user/bottom_nav_bar.dart
@@ -13,53 +13,6 @@ class _BottomNavComponentState extends State<BottomNavComponent> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 0.0),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
-              color: Colors.deepPurple.shade900,
-            ),
-            child: IconButton(
-              onPressed: () {},
-              icon: Image.asset("lib/assets/animated/menu2.gif"),
-            ),
-          ),
-        ),
-        title: Text("DASHBOARD",
-            style: GoogleFonts.getFont(
-              'Orbitron',
-              fontSize: 25,
-              color: const Color.fromARGB(255, 255, 255, 255),
-              fontWeight: FontWeight.bold,
-            )),
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.deepPurple.shade900,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 10.0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(90),
-                color: const Color.fromARGB(255, 255, 255, 255),
-              ),
-              height: 60,
-              width: 60,
-              child: IconButton(
-                icon: Image.asset(
-                  "lib/assets/animated/profile.gif",
-                  fit: BoxFit.cover,
-                  height: 60,
-                  width: 60,
-                ),
-                onPressed: () {},
-                color: const Color.fromARGB(255, 255, 255, 255),
-              ),
-            ),
-          ),
-        ],
-      ),
       backgroundColor: Colors.deepPurple.shade900,
       bottomNavigationBar: CurvedNavigationBar(
         backgroundColor: Colors.deepPurple.shade900,
--- lib/views/user/dashboard.dart
+++ lib/views/user/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class UserDashBoard extends StatefulWidget {
   const UserDashBoard({super.key});
@@ -10,9 +11,54 @@ class UserDashBoard extends StatefulWidget {
 class _UserDashBoardState extends State<UserDashBoard> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text("User Dashboard"),
-    ));
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 0.0),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              color: Colors.deepPurple.shade900,
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: Image.asset("lib/assets/animated/menu2.gif"),
+            ),
+          ),
+        ),
+        title: Text("DASHBOARD",
+            style: GoogleFonts.getFont(
+              'Orbitron',
+              fontSize: 25,
+              color: const Color.fromARGB(255, 255, 255, 255),
+              fontWeight: FontWeight.bold,
+            )),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.deepPurple.shade900,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(90),
+                color: const Color.fromARGB(255, 255, 255, 255),
+              ),
+              height: 60,
+              width: 60,
+              child: IconButton(
+                icon: Image.asset(
+                  "lib/assets/animated/profile.gif",
+                  fit: BoxFit.cover,
+                  height: 60,
+                  width: 60,
+                ),
+                onPressed: () {},
+                color: const Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
