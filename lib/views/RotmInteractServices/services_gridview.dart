cf10b6804fdb94881839367ad0083f40193f150d
Nallu-Swami <aringopalan13489@gmail.com>
2024-01-19 18:52:38 +0530

    Constants added

--- lib/views/RotmInteractServices/services_gridview.dart
+++ lib/views/RotmInteractServices/services_gridview.dart
@@ -46,7 +46,7 @@ class _InteractiveServicesState extends State<InteractiveServices> {
                           style: GoogleFonts.getFont(
                             'Orbitron',
                             fontSize: 20,
-                            color: const Color.fromARGB(255, 255, 255, 255),
+                            color: const Color.fromARGB(255, 0, 0, 0),
                             fontWeight: FontWeight.bold,
                           ),
                         ),
@@ -56,7 +56,7 @@ class _InteractiveServicesState extends State<InteractiveServices> {
                             style: GoogleFonts.getFont(
                               'Orbitron',
                               fontSize: 20,
-                              color: const Color.fromARGB(255, 255, 255, 255),
+                              color: const Color.fromARGB(255, 0, 0, 0),
                               fontWeight: FontWeight.bold,
                             ),
                           ),
@@ -82,32 +82,103 @@ class _InteractiveServicesState extends State<InteractiveServices> {
             ),
             height: 100,
             width: 100,
-            child: Image.asset(
-              "lib/assets/animated/learning-unscreen.gif",
-              height: 100,
-              width: 100,
+            child: Column(
+              children: [
+                Image.asset(
+                  "lib/assets/animated/learning-unscreen.gif",
+                  height: 110,
+                  width: 110,
+                ),
+                Row(
+                  children: [
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "HEALTH",
+                          style: GoogleFonts.getFont(
+                            'Orbitron',
+                            fontSize: 19,
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Center(
+                          child: Text(
+                            "INFORMATION",
+                            style: GoogleFonts.getFont(
+                              'Orbitron',
+                              fontSize: 19,
+                              color: const Color.fromARGB(255, 0, 0, 0),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
         Padding(
           padding: const EdgeInsets.all(10.0),
           child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.black,
-                width: 4,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 4,
+                ),
+                borderRadius: BorderRadius.circular(30),
+                color: const Color.fromARGB(255, 0, 221, 255),
               ),
-              borderRadius: BorderRadius.circular(30),
-              color: const Color.fromARGB(255, 0, 221, 255),
-            ),
-            height: 100,
-            width: 100,
-            child: Image.asset(
-              "lib/assets/animated/chat-bot-unscreen.gif",
               height: 100,
               width: 100,
-            ),
-          ),
+              child: Column(
+                children: [
+                  Image.asset(
+                    "lib/assets/animated/chat-bot-unscreen.gif",
+                    height: 110,
+                    width: 110,
+                  ),
+                  Center(
+                    child: Row(
+                      children: [
+                        const SizedBox(
+                          width: 40,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "HEALTH",
+                              style: GoogleFonts.getFont(
+                                'Orbitron',
+                                fontSize: 19,
+                                color: const Color.fromARGB(255, 0, 0, 0),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Center(
+                              child: Text(
+                                "BOT",
+                                style: GoogleFonts.getFont(
+                                  'Orbitron',
+                                  fontSize: 19,
+                                  color: const Color.fromARGB(255, 0, 0, 0),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )),
         ),
         Padding(
           padding: const EdgeInsets.all(10.0),
@@ -122,10 +193,47 @@ class _InteractiveServicesState extends State<InteractiveServices> {
             ),
             height: 100,
             width: 100,
-            child: Image.asset(
-              "lib/assets/animated/turing-test-unscreen.gif",
-              height: 100,
-              width: 100,
+            child: Column(
+              children: [
+                Image.asset(
+                  "lib/assets/animated/turing-test-unscreen.gif",
+                  height: 110,
+                  width: 110,
+                ),
+                Center(
+                  child: Row(
+                    children: [
+                      const SizedBox(
+                        width: 35,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "REPORT",
+                            style: GoogleFonts.getFont(
+                              'Orbitron',
+                              fontSize: 19,
+                              color: const Color.fromARGB(255, 0, 0, 0),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              "ANALYSIS",
+                              style: GoogleFonts.getFont(
+                                'Orbitron',
+                                fontSize: 19,
+                                color: const Color.fromARGB(255, 0, 0, 0),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ),
