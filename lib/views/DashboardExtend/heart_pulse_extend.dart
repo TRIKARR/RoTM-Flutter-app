82e29c8452081c4341191cda5ef02a93e7ce8dff
Nallu-Swami <aringopalan13489@gmail.com>
2024-01-27 01:39:10 +0530

    Segemented sliders

--- lib/views/DashboardExtend/heart_pulse_extend.dart
+++ lib/views/DashboardExtend/heart_pulse_extend.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 class UserHeartPulseStatExdended extends StatefulWidget {
   const UserHeartPulseStatExdended({super.key});
 
@@ -9,22 +12,94 @@ class UserHeartPulseStatExdended extends StatefulWidget {
 
 class _UserHeartPulseStatExdendedState
     extends State<UserHeartPulseStatExdended> {
+  int? _sliding = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your App Title'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(
-                context); // This will pop the current screen and go back.
-          },
+        appBar: AppBar(
+          title: Text(
+            'HEART PULSE',
+            style: GoogleFonts.getFont(
+              'Orbitron',
+              fontSize: 25,
+              color: const Color.fromARGB(255, 0, 0, 0),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(
+                  context); // This will pop the current screen and go back.
+            },
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Image.asset(
+                "lib/assets/animated/heartbeat.gif",
+                height: 40,
+                width: 40,
+              ),
+            )
+          ],
         ),
-      ),
-      body: const Center(
-        child: Text("Random Testing"),
-      ),
-    );
+        body: Center(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.deepPurple.shade900,
+            child: Column(
+              children: [
+                CupertinoSlidingSegmentedControl(
+                  children: {
+                    0: Text(
+                      'HOUR',
+                      style: GoogleFonts.getFont(
+                        'Orbitron',
+                        fontSize: 15,
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    1: Text(
+                      'HOUR',
+                      style: GoogleFonts.getFont(
+                        'Orbitron',
+                        fontSize: 15,
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    2: Text(
+                      'DAY',
+                      style: GoogleFonts.getFont(
+                        'Orbitron',
+                        fontSize: 15,
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    3: Text(
+                      'WEEK',
+                      style: GoogleFonts.getFont(
+                        'Orbitron',
+                        fontSize: 15,
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  },
+                  groupValue: _sliding,
+                  onValueChanged: (int? newValue) {
+                    setState(() {
+                      _sliding = newValue;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
