70f081bcd962af33a9244790820d1f889a0d52c8
Nallu-Swami <aringopalan13489@gmail.com>
2024-01-29 22:20:17 +0530

    Added Temp Graph

--- lib/views/DashboardExtend/temperature_extended.dart
+++ lib/views/DashboardExtend/temperature_extended.dart
@@ -2,6 +2,7 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rotm/views/dashboardSections/Section3Services/tempchart.dart';
 
 class UserTemperatureStatExtended extends StatefulWidget {
   const UserTemperatureStatExtended({super.key});
@@ -119,70 +120,7 @@ class _UserTemperatureStatExtendedState
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(0.0),
-                    child: LineChart(
-                      LineChartData(
-                        lineTouchData: const LineTouchData(
-                          enabled: true,
-                          touchTooltipData: LineTouchTooltipData(
-                            tooltipBgColor: Color.fromARGB(255, 255, 255, 255),
-                          ),
-                        ),
-                        gridData: const FlGridData(
-                          show: true,
-                          drawVerticalLine: true,
-                          drawHorizontalLine: true,
-                        ),
-                        // will work on FLtiles data later
-                        titlesData: const FlTitlesData(show: false),
-                        borderData: FlBorderData(
-                          show: true,
-                          border: Border.all(
-                              color: const Color.fromARGB(255, 0, 0, 0)),
-                        ),
-                        minX: 0,
-                        maxX: 16,
-                        minY: 0,
-                        maxY: 10,
-
-                        lineBarsData: [
-                          LineChartBarData(
-                            color: const Color.fromARGB(255, 0, 0, 0),
-                            spots: [
-                              //Random values for showcase
-                              const FlSpot(0, 0),
-                              const FlSpot(1, 1),
-                              const FlSpot(2, 4),
-                              const FlSpot(3, 2),
-                              const FlSpot(4, 5),
-                              const FlSpot(5, 4),
-                              const FlSpot(6, 3),
-                              const FlSpot(7, 4),
-                              const FlSpot(8, 3),
-                              const FlSpot(9, 4),
-                              const FlSpot(10, 3),
-                              const FlSpot(11, 5),
-                              const FlSpot(12, 3),
-                              const FlSpot(13, 5),
-                              const FlSpot(14, 4),
-                              const FlSpot(15, 2),
-                              const FlSpot(16, 1),
-                            ],
-                            show: true,
-                            isCurved: true,
-                            barWidth: 2.5,
-                            belowBarData: BarAreaData(
-                              show: true,
-                            ),
-                            aboveBarData: BarAreaData(show: false),
-                            dotData: const FlDotData(show: false),
-                            shadow: const Shadow(
-                              blurRadius: 0,
-                              color: Color.fromARGB(255, 0, 0, 0),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                    child: UserTemperatureGraph(),
                   ),
                 ),
                 const SizedBox(
