180f0965face8717aa5d50fc6a865a45c387b0bd
Nallu-Swami <aringopalan13489@gmail.com>
2024-02-20 23:40:31 +0530

    Final Measuers Added

--- lib/userdata.dart
+++ lib/userdata.dart
@@ -10,3 +10,4 @@ var UserPulseData;
 var UserOxyData;
 var EndPoint =
     "https://49d9-2401-4900-7b91-5ca8-6473-5d15-aa19-d6e.ngrok-free.app/";
+// Add your own End Points this is a Tunnel , you wont get anything hehe
